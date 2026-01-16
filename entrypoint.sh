5517a92aaa0ae8f9c101f421821707807ea7807c
tiagosilva7gamespc-dotcom <tiagosilva7gamespc@gmail.com>
2026-01-16 11:04:30 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-echo "--- Iniciando Operário 02 (Traffmonetizer) ---"
+echo "--- Iniciando Operário 02 (Render Node) ---"
 
-# Caminho nativo da imagem oficial
-if [ -f "/app/Cli" ]; then
-    echo "✅ Binário nativo localizado."
-    # Comando 'accept' conforme a documentação que você enviou
-    /app/Cli start accept --token "f99SPwO2pZowxY5M2vU2MVQYEVlprfAJQdBhby7zJsc=" --device-name "HF_Master_Node" &
-    echo "🚀 Traffmonetizer em background!"
+# 1. Inicia o Traffmonetizer em background
+if [ -f "./Cli" ]; then
+    echo "✅ Binário nativo detectado."
+    ./Cli start accept --token "f99SPwO2pZowxY5M2vU2MVQYEVlprfAJQdBhby7zJsc=" --device-name "Render_Master_Node" &
+    echo "🚀 Traffmonetizer disparado!"
 else
     echo "❌ Erro: Binário Cli não encontrado!"
     exit 1
 fi
 
-# Inicia o disfarce IMEDIATAMENTE. O Hugging Face precisa disso na porta 7860.
-echo "--- Abrindo Porta 7860 (Gradio) ---"
-python3 /app/app.py
-
-echo "--- Teste de Conectividade ---"
-# Tenta ver se a porta 769 está aberta no servidor deles
-curl -v telnet://64.34.81.49:769 --connect-timeout 5
+# 2. Servidor de "Disfarce" Ultraleve
+# Isso responde na porta 10000 para a Render saber que o app está vivo
+echo "--- Iniciando Servidor de Health Check (Porta 10000) ---"
+python3 -m http.server 10000
