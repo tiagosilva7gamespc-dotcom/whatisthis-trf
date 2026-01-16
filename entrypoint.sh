0b7a0a694a317bd47c688a7a87663565f317dc9f
tiagosilva7gamespc-dotcom <tiagosilva7gamespc@gmail.com>
2026-01-16 11:25:54 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-echo "--- Iniciando Operário 02 (Render Node) ---"
+echo "--- Iniciando Operário 02 (Render Master) ---"
 
 # 1. Inicia o Traffmonetizer com o seu Token
-# Usamos o comando 'accept' validado anteriormente
 if [ -f "./Cli" ]; then
-    echo "✅ Binário nativo localizado. Iniciando compartilhamento..."
-    ./Cli start accept --token "f99SPwO2pZowxY5M2vU2MVQYEVlprfAJQdBhby7zJsc=" --device-name "Render_Master_Node" &
-    echo "🚀 Traffmonetizer disparado em background!"
+    echo "✅ Binário nativo localizado."
+    # Usamos o seu token fixo conforme a documentação
+    ./Cli start accept --token "f99SPwO2pZowxY5M2vU2MVQYEVlprfAJQdBhby7zJsc=" --device-name "Render_Node_01" &
+    echo "🚀 Traffmonetizer disparado!"
 else
     echo "❌ Erro: Binário Cli não encontrado!"
     exit 1
 fi
 
-# 2. Servidor de Health Check (Ultraleve)
-# Binda explicitamente em 0.0.0.0 na porta 10000
-echo "--- Iniciando Servidor de Status (Porta 10000) ---"
-python3 -m http.server 10000 --bind 0.0.0.0
+# 2. Servidor de Status com Porta Dinâmica
+# Se a Render não mandar uma porta, usamos a 10000 como backup
+RENDER_PORT=${PORT:-10000}
+
+echo "--- Abrindo Porta de Health Check: $RENDER_PORT ---"
+python3 -m http.server $RENDER_PORT --bind 0.0.0.0
